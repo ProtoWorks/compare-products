7c69efe42c374d2b3b8d63a92f53ecce38c37a88
Peter Farkas <peter@semmiseg.info>
2020-04-30 12:19:38 +0200

    Fix docs folder content

---
+++ docs/build/precache-manifest.fa17dcb2906a632c8a88aff69d3fa3d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e45bd52ff62bec92c25fe6b09bdf081",
+    "url": "/compare-products/index.html"
+  },
+  {
+    "revision": "5bd3ed994b120d2cccda",
+    "url": "/compare-products/static/css/main.3c679886.chunk.css"
+  },
+  {
+    "revision": "0ae7e5e751fd27314835",
+    "url": "/compare-products/static/js/2.048787d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/compare-products/static/js/2.048787d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd3ed994b120d2cccda",
+    "url": "/compare-products/static/js/main.899cfc82.chunk.js"
+  },
+  {
+    "revision": "05941f54791b4a243233",
+    "url": "/compare-products/static/js/runtime-main.df83f515.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/compare-products/static/media/logo.be622899.svg"
+  }
+]);
